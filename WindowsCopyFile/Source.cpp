74adc5043f1faf98c31acdd8376b08214f56b5f2
TURRET14 <EmelyanenkoSemyon2006@ya.ru>
2025-01-24 00:13:22 +0300

    Добавьте файлы проекта.

---
+++ WindowsCopyFile/Source.cpp
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		printf("Not 2 Files Were Given!");
+		return 1;
+	}
+
+	int Size1 = MultiByteToWideChar(CP_UTF8, 0, argv[1], -1, nullptr, 0);
+	wchar_t* File1 = new wchar_t[Size1];
+	MultiByteToWideChar(CP_UTF8, 0, argv[1], -1, File1, Size1);
+
+	int Size2 = MultiByteToWideChar(CP_UTF8, 0, argv[2], -1, nullptr, 0);
+	wchar_t* File2 = new wchar_t[Size2];
+	MultiByteToWideChar(CP_UTF8, 0, argv[2], -1, File2, Size2);
+
+	if (!CopyFile(File1, File2, FALSE)) {
+		printf("Copy Error. %d", GetLastError());
+		return 1;
+	}
+	return 0;
+}
